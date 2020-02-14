7e74b506618317cdb567021ded097cf32eacc31b
Abdul hady Akash <47117532+abdulhadyakash@users.noreply.github.com>
2020-02-14 21:48:09 +0600

    Reg
    Student REG Form

---
+++ welcome.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>HTML Table</title>
+</head>
+<body bgcolor='7136A9'>
+<form method="" action="">
+<table border="2" align="center" width="1000" height="600" bgcolor="#490766" >
+<caption><h1>Course Registration form</h1></caption>
+<tr>
+<th><h3>Course Name</h3></th>
+<td><input type="text" name="fn" id="fn1" maxlength="10" title="enter your first name" placeholder="enter your Course Name" required/></td>
+</tr>
+<tr>
+<th><h3>Code</h3></th>
+<td><input type="string"/></td>
+</tr>
+<tr>
+<th><h3>Enter your password</h3></th>
+<td><input type="password"/></td>
+</tr>
+<tr>
+<th><h3>Enter your mobile</h3></th>
+<td><input type="number"/></td>
+</tr>
+<tr>
+<td colspan="10" align="center"><input type="submit" value="Save My Data"/>
+<input type="reset" value="Reset Data"/>
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
